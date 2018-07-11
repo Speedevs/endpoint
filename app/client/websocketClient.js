5ed1a145f89956ef301c5e6dbc81766db2300a0b
ProphetDaniel <prophetdaniel@ethereumclassic.org>
2018-07-11 08:14:57 -0300

    :construction: Development path

--- app/client/websocketClient.js
+++ app/client/websocketClient.js
@@ -5,8 +5,8 @@ var socket = io.connect('http://localhost:8080', {
 });
 
 socket.emit('getMarket', {
-  token: '0x000',
-  user: void 0,
+  token: '0xB561fEF0d624C0826ff869946f6076B7c4f2ba42',
+  user: '0x9343938289b0673ffC6DDb905316F52FFE24eB94',
 });
 socket.once('market', function(
   result
--- app/server/websocketServer.js
+++ app/server/websocketServer.js
@@ -3,6 +3,34 @@ import socket_io from 'socket.io';
 
 const PORT = 8080;
 
+// { tokenAddr: '0x8f3470a7388c05ee4e7af3d01d8c722b0ff52374',
+//   quoteVolume: 1000.1,
+//   baseVolume: 212.3,
+//   last: 0.245,
+//   percentChange: 0.0047,
+//   bid: 0.243,
+//   ask: 0.246 },
+function ReturnTickerItem(_tokenAddr, _quoteVolume, _baseVolume, _last, _percentChange, _bid, _ask){
+  this.tokenAddr = _tokenAddr;
+  this.quoteVolume = _quoteVolume;
+  this.baseVolume = _baseVolume;
+  this.last = _last;
+  this.percentChange = _percentChange;
+  this.bid = _bid;
+  this.ask = _ask;
+}
+function aReturnTickerItem(name, _tokenAddr, _quoteVolume, _baseVolume, _last, _percentChange, _bid, _ask){
+  this['ETH_'+name] = new ReturnTickerItem(_tokenAddr, _quoteVolume, _baseVolume, _last, _percentChange, _bid, _ask);
+}
+function Market(_returnTicker, _orders, _trades, _myOrders, _myTrades, _myFunds) {
+  this.returnTicker = _returnTicker;
+  this.orders = _orders;
+  this.trades = _trades;
+  this.myOrders = _myOrders;
+  this.myTrades = _myTrades;
+  this.myFunds = _myFunds;
+}
+
 Meteor.startup(() => {
   // Server
   const server = http.createServer();
@@ -19,7 +47,7 @@ Meteor.startup(() => {
       console.log('Server received get market request.');
       console.log(JSON.stringify(token));
       console.log(JSON.stringify(user));
-      socket.emit('market', 'The market');
+      socket.emit('market', new Market(new aReturnTickerItem('VIRI', '0x8f3470a7388c05ee4e7af3d01d8c722b0ff52374', 1000.1, 212.3, 0.245, 0.0047, 0.243, 0.246)));
     });
   });
 
